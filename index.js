05e9c76dbdf8da5d5e6b54d76f0f6f7b0f0e46b7
Maksim Hvatov <uzhas76064@gmail.com>
2019-05-30 16:53:23 +0300

    added all main files

---
+++ index.js
@@ -0,0 +1,8 @@
+const path = require('path');
+const url = require('url');
+
+const {app, BrowserWindow} = require('electron');
+
+function createWindow() {
+    let win = new BrowserWindow({width: 700, height: 700});
+}
